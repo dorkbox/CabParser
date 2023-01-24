fcc3d82e51babf248cc0fa231c01be8bdcb8441b
Robinson <robinson@dorkbox.com>
2023-01-24 18:27:47 +0100

    added JPMS MRJAR support

---
+++ src9/module-info.java
@@ -0,0 +1,16 @@
+module com.dorkbox.cab_parser {
+    exports dorkbox.cabParser;
+    exports dorkbox.cabParser.decompress;
+    exports dorkbox.cabParser.decompress.lzx;
+    exports dorkbox.cabParser.decompress.none;
+    exports dorkbox.cabParser.decompress.zip;
+    exports dorkbox.cabParser.extractor;
+    exports dorkbox.cabParser.structure;
+
+
+    requires transitive dorkbox.bytes;
+    requires transitive dorkbox.updates;
+    requires transitive dorkbox.utilities;
+
+    requires transitive kotlin.stdlib;
+}
